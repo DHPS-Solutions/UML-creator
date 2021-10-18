f824e5c38b344556edc98b170a4a679ea6b93999
lytixdev <nicolaihbrand@gmail.com>
2021-10-18 11:04:34 +0200

    added a way to read files

---
+++ file_reader.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    FILE *fptr;
+    char filename[15] = "tests/Test.java";
+    char ch;
+
+    fptr = fopen(filename, "r");
+    if (fptr == NULL) {
+        printf("Cannot open file\n");
+        exit(0);
+    }
+    ch = fgetc(fptr);
+    while (ch != EOF) {  // EOF -> end of file
+        printf("%c", ch);
+        ch = fgetc(fptr);
+    }
+    fclose(fptr);
+}
---
+++ tests/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is not greater than b");
+    }
+}
